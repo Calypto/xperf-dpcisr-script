5a79abf1da5430021a1b0275f64aa22e5b486148
Calypto <mikhailichenko77@gmail.com>
2020-11-19 03:28:11 -0500

    Add files via upload

--- xperf.bat
+++ xperf.bat
@@ -1,5 +1,14 @@
+@echo off
 cd C:\Program Files\Microsoft Windows Performance Toolkit
+echo Starting in 5 seconds.
+timeout 5
+echo(
+echo Recording...
 xperf -on base+interrupt+dpc
 timeout 5
 xperf -stop
 xperf -i c:\kernel.etl -o c:\report.txt -a dpcisr
+echo(
+echo Done.
+echo(
+cmd /k
